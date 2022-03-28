a3ed20ae8489a086bed41fe44e59d816a4bae65b
Jeremy Durden <jeremydurden@gmail.com>
2022-03-28 16:22:12 -0700

    landing page basics

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,6 +2,27 @@ import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
 
 const Landing = () => {
-  return <h4>Landing Page</h4>;
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            Job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+            perferendis nulla reprehenderit eligendi laudantium! Expedita
+            placeat nihil explicabo! Non numquam sapiente aspernatur itaque! Aut
+            voluptate impedit dolore deserunt nulla! Magnam.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </main>
+  );
 };
 export default Landing;
