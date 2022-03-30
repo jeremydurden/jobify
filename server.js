f0202e04543393f731af6b498a5f1bf191f62e7b
Jeremy Durden <jeremydurden@gmail.com>
2022-03-29 19:04:19 -0700

    adding errorHandlerMiddleware to server

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const app = express();
 
 //middleware
 import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
 
 app.get("/", (req, res) => {
   res.send("Welcome!");
 });
 
 app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 8080;
 
