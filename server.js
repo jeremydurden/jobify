6e7db50fb9333b7d5dbb3c05051ff884ebec3574
Jeremy Durden <jeremydurden@gmail.com>
2022-03-31 17:04:01 -0700

    setting up connection to mongoDB and 404 and 500 status middleware for dealing with errors

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 import express from "express";
 const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+
+import connectDB from "./db/connect.js";
 
 //middleware
 import notFoundMiddleware from "./middleware/not-found.js";
@@ -14,6 +18,15 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}...`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
