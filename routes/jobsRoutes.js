3dea24bd67ebe39702f1ee38a7998d13ef12b87b
Jeremy Durden <jeremydurden@gmail.com>
2022-04-01 17:28:36 -0700

    adding User model and updating authController

--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -12,6 +12,6 @@ import {
 router.route("/").post(createJob).get(getAllJobs);
 // remember :id
 router.route("/stats").get(showStats);
-router.route("/id").delete(deleteJob).patch(updateJob);
+router.route("/:id").delete(deleteJob).patch(updateJob);
 
 export default router;
