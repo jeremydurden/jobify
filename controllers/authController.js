f2cb8450103caf7c39201aeeea02638fda1b48e6
Jeremy Durden <jeremydurden@gmail.com>
2022-04-03 13:07:33 -0700

    using custom Error class in the auth register controller to handle status code 400 errors when a field is left blank in the registraction form

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,23 @@
 import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
 
+//extending from Error class to add a cust statusCode property that can be used for a logic check in the errorHandlerMiddlerware function
+class CustomAPIError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
 const register = async (req, res) => {
-  const user = await User.create(req.body);
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    //manually throwing a custom Error if any of these values are missing from the req.body
+    throw new CustomAPIError("please provide all values");
+  }
+
+  const user = await User.create({ name, email, password });
   res.status(StatusCodes.CREATED).json({ user });
 };
 const login = async (req, res) => {
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,14 +1,14 @@
 import { StatusCodes } from "http-status-codes";
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err);
   const defaultError = {
-    StatusCodes: StatusCodes.INTERNAL_SERVER_ERROR,
-    msg: "Something went wrong—try again later.",
+    //checking for value from the custom Error used in the controller or using the http-status-codes package
+    StatusCodes: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong—try again later.",
   };
   if (err.name === "ValidationError") {
     defaultError.StatusCodes = StatusCodes.BAD_REQUEST;
-    // defaultError.msg = err.message;
+    // returns an array from Object.values(err.errors) and then maps over the array of the object's values to join any missing-input, message strings
     defaultError.msg = Object.values(err.errors)
       .map((item) => item.message)
       .join(",");
