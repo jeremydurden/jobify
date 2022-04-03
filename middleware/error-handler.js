f9969e57a5c8aca2aababbf50dedfec8b17c71de
Jeremy Durden <jeremydurden@gmail.com>
2022-04-02 18:16:21 -0700

    adding http-status-codes package and updating errorHandlerMiddleware to respond with correct status code and msg for missing registration input data

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,20 @@
+import { StatusCodes } from "http-status-codes";
+
 const errorHandlerMiddleware = (err, req, res, next) => {
   console.log(err);
-  res.status(500).json({ msg: err });
+  const defaultError = {
+    StatusCodes: StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: "Something went wrong—try again later.",
+  };
+  if (err.name === "ValidationError") {
+    defaultError.StatusCodes = StatusCodes.BAD_REQUEST;
+    // defaultError.msg = err.message;
+    defaultError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+  }
+  // res.status(defaultError.StatusCodes).json({ msg: err });
+  res.status(defaultError.StatusCodes).json({ msg: defaultError.msg });
 };
 
 export default errorHandlerMiddleware;
