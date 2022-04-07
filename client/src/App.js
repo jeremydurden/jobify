9e5797eb438848b441101cb0abeea08f69d3fa47
Jeremy Durden <jeremydurden@gmail.com>
2022-04-07 14:45:44 -0700

    setting up shared dashboard layout on the front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import { Landing, Dashboard, Register, Error } from "./pages";
+
+import {
+  AddJob,
+  Profile,
+  AllJobs,
+  SharedLayout,
+  Stats,
+} from "./pages/dashboard";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<Stats />} />
+            <Route path="all-jobs" element={<AllJobs />} />
+            <Route path="add-job" element={<AddJob />} />
+            <Route path="profile" element={<Profile />} />
+          </Route>
           <Route path="/register" element={<Register />} />
           <Route path="/landing" element={<Landing />} />
           <Route path="*" element={<Error />} />
