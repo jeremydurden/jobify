ab70e8eab3c4a243763f5f52c6da9fcf4f957193
Jeremy Durden <jeremydurden@gmail.com>
2022-04-08 11:52:48 -0700

    small sidebar structure as well as a links util file

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,10 +1,31 @@
+import { useAppContext } from "../context/appContext";
+import { NavLink } from "react-router-dom";
+import links from "../utils/links";
+import { FaTimes } from "react-icons/fa";
+import Logo from "./Logo";
 import Wrapper from "../assets/wrappers/SmallSidebar";
 
-const SmallSidebar = () => {
+export const SmallSidebar = () => {
+  const { showSidebar, toggleSidebar } = useAppContext();
   return (
     <Wrapper>
-      <h4>Small Sidebar</h4>
+      <div className="sidebar-container show-sidebar">
+        <div className="content">
+          <button
+            type="button"
+            className="close-btn"
+            onClick={() => console.log("toggle")}
+          >
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">nav links</div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
+
 export default SmallSidebar;
