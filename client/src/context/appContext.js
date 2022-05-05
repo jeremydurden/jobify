a20063fcada80e089ce514f64696bb69e5c90a91
Jeremy Durden <jeremydurden@gmail.com>
2022-05-05 15:26:02 -0700

    setting up form and local state on the Profile page on the dashboard

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -91,6 +91,10 @@ const AppProvider = ({ children }) => {
     removeUserFromLocaleStorage();
   };
 
+  const updateUser = async (currentUser) => {
+    console.log(currentUser);
+  };
+
   return (
     <AppContext.Provider
       value={{
@@ -99,6 +103,7 @@ const AppProvider = ({ children }) => {
         setupUser,
         toggleSidebar,
         logoutUser,
+        updateUser,
       }}
     >
       {children}
--- client/src/pages/dashboard/Profile.js
+++ client/src/pages/dashboard/Profile.js
@@ -1,4 +1,71 @@
-function Profile() {
-  return <div>Profile</div>;
-}
+import { useState } from "react";
+import { FormRow, Alert } from "../../components";
+import { useAppContext } from "../../context/appContext";
+import Wrapper from "../../assets/wrappers/DashboardFormPage";
+
+const Profile = () => {
+  //destructuring form the useAppContext
+  const { user, showAlert, displayAlert, updateUser, isLoading } =
+    useAppContext();
+
+  //setting default value based on the user in the global state
+  const [name, setName] = useState(user?.name);
+  const [email, setEmail] = useState(user?.email);
+  const [lastName, setLastName] = useState(user?.lastName);
+  const [location, setLocation] = useState(user?.location);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //remove while testing
+    if (!name || !email || !lastName || !location) {
+      displayAlert();
+      return;
+    }
+    updateUser({ name, email, lastName, location });
+  };
+
+  return (
+    <Wrapper>
+      <form onSubmit={handleSubmit} className="form">
+        <h3>profile</h3>
+        {showAlert && <Alert />}
+        {/* {name} */}
+        <div className="form-center">
+          <FormRow
+            labelText="Name"
+            type="text"
+            name="name"
+            value={name}
+            handleChange={(e) => setName(e.target.value)}
+          />
+          <FormRow
+            labelText="Last Name"
+            type="text"
+            name="name"
+            value={lastName}
+            handleChange={(e) => setLastName(e.target.value)}
+          />
+          <FormRow
+            labelText="Email"
+            type="email"
+            name="email"
+            value={email}
+            handleChange={(e) => setEmail(e.target.value)}
+          />
+          <FormRow
+            labelText="Location"
+            type="text"
+            name="location"
+            value={location}
+            handleChange={(e) => setLocation(e.target.value)}
+          />
+          <button className="btn btn-block" type="submit" disabled={isLoading}>
+            {isLoading ? "Please wait..." : "save changes"}
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
+};
+
 export default Profile;
