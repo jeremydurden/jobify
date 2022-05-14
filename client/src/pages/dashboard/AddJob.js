b69269fc6294053ae7ba0a75f10d310e6273c1f9
Jeremy Durden <jeremydurden@gmail.com>
2022-05-13 17:49:03 -0700

    setting up the structure for the AddJob page

--- client/src/pages/dashboard/AddJob.js
+++ client/src/pages/dashboard/AddJob.js
@@ -1,4 +1,78 @@
+import { FormRow, Alert } from "../../components";
+import { useAppContext } from "../../context/appContext";
+import Wrapper from "../../assets/wrappers/DashboardFormPage";
+import { useState } from "react";
+
 function AddJob() {
-  return <div>AddJob</div>;
+  const {
+    showAlert,
+    displayAlert,
+    position,
+    company,
+    jobLocation,
+    jobTpye,
+    jobTypeOptions,
+    status,
+    statusOptions,
+    isEditing,
+  } = useAppContext();
+
+  const handleJobInput = (e) => {
+    const { name, value } = e.target;
+    console.log(`${name}: ${value}`);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!position || !company || !jobLocation) {
+      displayAlert();
+      return;
+    }
+  };
+
+  return (
+    <Wrapper>
+      <form className="form">
+        <h3>{isEditing ? "edit job" : "add job"}</h3>
+        {showAlert && <Alert />}
+        <div className="form-center">
+          <FormRow
+            labelText="Position"
+            type="text"
+            name="position"
+            value={position}
+            handleChange={handleJobInput}
+          />
+          {/* company */}
+          <FormRow
+            labelText="Company"
+            type="text"
+            name="company"
+            value={company}
+            handleChange={handleJobInput}
+          />
+          {/* location */}
+          <FormRow
+            type="text"
+            labelText="Job Location"
+            name="jobLocation"
+            value={jobLocation}
+            handleChange={handleJobInput}
+          />
+          {/* JOB TYPE */}
+          {/* JOB STATUS */}
+          <div className="btn-container">
+            <button
+              onClick={handleSubmit}
+              type="submit"
+              className="btn btn-block submit-btn"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </Wrapper>
+  );
 }
 export default AddJob;
