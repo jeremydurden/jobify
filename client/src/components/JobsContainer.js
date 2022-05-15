f290a4a4eb8fce55818842951dc29de232b92eee
Jeremy Durden <jeremydurden@gmail.com>
2022-05-15 16:25:45 -0700

    mapping over all jobs in the JobsContainer and displaying some of the info on the Job component

--- client/src/components/JobsContainer.js
+++ client/src/components/JobsContainer.js
@@ -11,10 +11,35 @@ const JobsContainer = () => {
   useEffect(() => {
     getJobs();
   }, []);
+
+  if (isLoading) {
+    return (
+      <div>
+        <Loading center />
+      </div>
+    );
+  }
+
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display...</h2>
+      </Wrapper>
+    );
+  }
+
   return (
-    <div>
-      <Loading center />
-    </div>
+    <Wrapper>
+      <h5>
+        {totalJobs} job{jobs.length > 1 && "s"} found
+      </h5>
+      <div className="jobs">
+        {jobs.map((job) => {
+          return <Job key={job._id} {...job} />;
+        })}
+      </div>
+      {/* pagination buttons */}
+    </Wrapper>
   );
 };
 export default JobsContainer;
