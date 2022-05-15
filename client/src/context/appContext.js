a9c63dd3907fb02eb6d8321bfd940ba3a1ec3915
Jeremy Durden <jeremydurden@gmail.com>
2022-05-14 19:43:29 -0700

    setting up front-end function getJobs to be able to make a get request to the getAllJobs route

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -17,6 +17,8 @@ import {
   CREATE_JOB_BEGIN,
   CREATE_JOB_SUCCESS,
   CREATE_JOB_ERROR,
+  GET_JOBS_BEGIN,
+  GET_JOBS_SUCCESS,
 } from "./actions";
 
 const token = localStorage.getItem("token");
@@ -41,6 +43,10 @@ export const initialState = {
   jobType: "full-time",
   statusOptions: ["interview", "declined", "pending"],
   staus: "pending",
+  jobs: [],
+  totalJobs: 0,
+  numOfPages: 1,
+  page: 1,
 };
 
 const AppContext = React.createContext();
@@ -193,6 +199,23 @@ const AppProvider = ({ children }) => {
     clearAlert();
   };
 
+  const getJobs = async () => {
+    let url = `/jobs`;
+    dispatch({ type: GET_JOBS_BEGIN });
+    try {
+      //axios setup makes the authFetch intance default to a get request but you can also use .get
+      const { data } = await authFetch(url);
+      const { jobs, totalJobs, numOfPages } = data;
+      dispatch({
+        type: GET_JOBS_SUCCESS,
+        payload: { jobs, totalJobs, numOfPages },
+      });
+    } catch (error) {
+      console.log(error.response);
+    }
+    clearAlert();
+  };
+
   return (
     <AppContext.Provider
       value={{
@@ -205,6 +228,7 @@ const AppProvider = ({ children }) => {
         handleChange,
         clearValues,
         createJob,
+        getJobs,
       }}
     >
       {children}
